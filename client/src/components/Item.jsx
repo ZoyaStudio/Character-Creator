a01462339ea469e13996ce5a7f2b3b9222ba52a4
Elizabeth Mendenhall <eamendenhall@outlook.com>
2021-09-20 13:50:14 -0700

    made code more "dry"

--- client/src/components/Item.jsx
+++ client/src/components/Item.jsx
@@ -1,43 +1,34 @@
 import React, {useState, useContext} from 'react';
 
 //   white: {brightness: 3.5, saturation: 1, hue: 0, opacity: 100, contrast: 144.5},
-const defaultFilter = "brightness(" + 3.5 + ") hue-rotate(" + 0 + 'deg) saturate(' + 1 + '%) contrast(' + 144.5 + '%) opacity(' + 100 + '%)'
+
 const makeFilterStyle = (hue, sat, brit, con, op) => {
   if (hue === undefined || sat === undefined || brit === undefined || con === undefined ||op === undefined) {
-    return decorationFilter;
+    return "brightness(" + 3.5 + ") hue-rotate(" + 0 + 'deg) saturate(' + 1 + '%) contrast(' + 144.5 + '%) opacity(' + 100 + '%)';
   }
   return "brightness(" + brit + ") hue-rotate(" + hue + 'deg) saturate(' + sat + '%) contrast(' + con + '%) opacity(' + op + '%)';
 }
 function Item(props) {
   let {base, seamUrls, decoration, upperBoundary, neckline, lowerBoundary, zCounter, baseFilter, decorationFilter, displayClass, onClickEvent} = props;
   // console.log(onClickEvent);
-  var decorationsArr = [];
-  var seamsArr = [];
-  if (seamUrls) {
-    seamsArr = seamUrls.map((url, index )=> <img src={url} className={displayClass} style={{zIndex: zCounter - 4}} key={index + "item"}/>)
-  }
+  var seamsArr = seamUrls ? seamUrls.map((url, index )=> <img src={url} className={displayClass} style={{zIndex: zCounter - 4}} key={index + "item"}/>) : [];
+
   if (decoration) {
-    if (decorationFilter) {
-      decorationFilter = makeFilterStyle(decorationFilter.hue, decorationFilter.sat, decorationFilter.brit, decorationFilter.con, decorationFilter.op);
-    } else {
-      decorationFilter = defaultFilter;
-    }
-    decorationsArr.push(<img src={decoration.outlineUrl} className={displayClass} style={{zIndex: zCounter}}/>);
-    decorationsArr.push(<img src={decoration.gradientUrl} className={displayClass} style={{zIndex: zCounter -1, filter: decorationFilter}}/>);
-    decorationsArr.push(<img src={decoration.shadingUrl} className={displayClass} style={{zIndex: zCounter -2, filter: decorationFilter}}/>);
+    let formattedDecorationFilter = decorationFilter ? makeFilterStyle(decorationFilter.hue, decorationFilter.sat, decorationFilter.brit, decorationFilter.con, decorationFilter.op) : makeFilterStyle();
+  var decorationsArr = [
+    <img src={decoration.outlineUrl} className={displayClass} style={{zIndex: zCounter}}/>,
+    <img src={decoration.gradientUrl} className={displayClass} style={{zIndex: zCounter -1, filter: formattedDecorationFilter}}/>,
+    <img src={decoration.shadingUrl} className={displayClass} style={{zIndex: zCounter -2, filter: formattedDecorationFilter}}/>
+  ];
   }
   var baseImgs = null;
   if (base) {
-    if (baseFilter) {
-      baseFilter = makeFilterStyle(baseFilter.hue, baseFilter.sat, baseFilter.brit, baseFilter.con, baseFilter.op);
-    } else {
-      baseFilter = defaultFilter;
-    }
+      let formattedFilter = baseFilter  ?  makeFilterStyle(baseFilter.hue, baseFilter.sat, baseFilter.brit, baseFilter.con, baseFilter.op) : makeFilterStyle();
     baseImgs = (
       <React.Fragment>
         <img src={base.outlineUrl} className={displayClass} style={{zIndex: zCounter -10}}/>
-        <img src={base.gradientUrl} className={displayClass} style={{zIndex: zCounter - 11, filter: baseFilter}}/>
-        <img src={base.shadingUrl} className={displayClass} style={{zIndex: zCounter - 12, filter: baseFilter}}/>
+        <img src={base.gradientUrl} className={displayClass} style={{zIndex: zCounter - 11, filter: formattedFilter}}/>
+        <img src={base.shadingUrl} className={displayClass} style={{zIndex: zCounter - 12, filter: formattedFilter}}/>
       </React.Fragment>
     )
   }
