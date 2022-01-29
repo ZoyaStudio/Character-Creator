0500b3db5a2fa637b7d99adf3c761883997848e6
Elizabeth Mendenhall <eamendenhall@outlook.com>
2022-01-28 23:03:23 -0800

    Modified types to accomodate changes made to filter names

--- src/components/body/HairPart.tsx
+++ src/components/body/HairPart.tsx
@@ -1,49 +1,54 @@
-import React from 'react';
-import {hairProfiles} from '../../utility/sample-data';
-import {HairObj, HairTypes, Color} from '../../utility/types';
-import {makeFilterStyle} from '../../utility/helper-functions';
+import React from 'react'
+import { hairProfiles } from '../../utility/sample-data'
+import { HairObj, HairTypes, Color } from '../../utility/types'
+import { makeFilterStyle } from '../../utility/helper-functions'
+
 type AppProps = {
-  part: HairObj,
-  type: HairTypes,
-  displayClass: string,
-  zCounter: number,
-  baseFilter: Color,
-  tintFilter: Color
+    part: HairObj
+    type: HairTypes
+    displayClass: string
+    zCounter: number
+    baseFilter: Color
+    tintFilter: Color
 }
 const HairPart = function hairPart({
-  part,
-  type,
-  displayClass,
-  // onClickHandler,
-  zCounter,
-  baseFilter,
-  tintFilter
-} : AppProps) {
-  const { outlineUrl, gradientUrl, shadingUrl } = hairProfiles[type][part.typeKey][part.index];
-  return (
-    <div
-      className={displayClass}
-      style={{ zIndex: zCounter - 1 }}
-    >
-      <img
-        alt="outline"
-        src={outlineUrl}
-        style={{ zIndex: zCounter - 1 }}
-        className={displayClass}
-      />
-      <img
-        alt="gradient"
-        src={gradientUrl}
-        style={{ zIndex: zCounter - 2, filter: makeFilterStyle(tintFilter) }}
-        className={displayClass}
-      />
-      <img
-        alt="shading"
-        src={shadingUrl}
-        style={{ zIndex: zCounter - 4, filter: makeFilterStyle(baseFilter) }}
-        className={displayClass}
-      />
-    </div>
-  );
-};
-export default HairPart;
+    part,
+    type,
+    displayClass,
+    // onClickHandler,
+    zCounter,
+    baseFilter,
+    tintFilter,
+}: AppProps): JSX.Element {
+    const { outlineUrl, gradientUrlA, shadingUrlA } =
+        hairProfiles[type][part.typeKey][part.index]
+    return (
+        <div className={displayClass} style={{ zIndex: zCounter - 1 }}>
+            <img
+                alt="outline"
+                src={outlineUrl}
+                style={{ zIndex: zCounter - 1 }}
+                className={displayClass}
+            />
+            <img
+                alt="gradient"
+                src={gradientUrlA}
+                style={{
+                    zIndex: zCounter - 2,
+                    filter: makeFilterStyle(tintFilter),
+                }}
+                className={displayClass}
+            />
+            <img
+                alt="shading"
+                src={shadingUrlA}
+                style={{
+                    zIndex: zCounter - 4,
+                    filter: makeFilterStyle(baseFilter),
+                }}
+                className={displayClass}
+            />
+        </div>
+    )
+}
+export default HairPart
